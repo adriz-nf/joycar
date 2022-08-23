cbc7b3900ff70f7fab209691c91fefcce4042662
Andrew Emmett <71984846+adriz-nf@users.noreply.github.com>
2022-08-23 19:54:46 +0200

    Show names on button press

--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
-basic.showLeds(`
-    # . # . #
-    # . # . .
-    # # # . #
-    # . # . #
-    # . # . #
-    `)
-basic.pause(500)
-basic.showString(" INGRID")
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . # . #
+        # . # . .
+        # # # . #
+        # . # . #
+        # . # . #
+        `)
+    basic.pause(500)
+    basic.showString(" INGRID")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # . # . #
+        # . # . .
+        # # # . #
+        # . # . #
+        # . # . #
+        `)
+    basic.pause(500)
+    basic.showString("FRIDA")
+})
 basic.forever(function () {
 	
 })
