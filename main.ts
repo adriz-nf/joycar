dc366d1b535d0970e054054494cc0e9cf9a00b36
Andrew Emmett <71984846+adriz-nf@users.noreply.github.com>
2022-08-23 19:34:45 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+basic.showLeds(`
+    # . # . #
+    # . # . .
+    # # # . #
+    # . # . #
+    # . # . #
+    `)
+basic.pause(500)
+basic.showString(" INGRID")
 basic.forever(function () {
 	
 })
