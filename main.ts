e667827f4ec27d1f657aad7b56dc2d2bb28cba4d
Andrew Emmett <71984846+adriz-nf@users.noreply.github.com>
2022-08-27 18:30:15 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,55 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
+    JoyCar.stop(StopIntensity.Intense)
     basic.showLeds(`
         # . # . #
-                # . # . .
-                # # # . #
-                # . # . #
-                # . # . #
-    `)
+        # . # . .
+        # # # . .
+        # . # . #
+        # . # . #
+        `)
     basic.pause(500)
     basic.showString(" INGRID")
 })
-input.onSound(DetectedSound.Loud, function on_sound_loud() {
+input.onSound(DetectedSound.Loud, function () {
+    basic.showLeds(`
+        # . . . .
+        # # # # #
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
     JoyCar.stop(StopIntensity.Intense)
 })
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+input.onButtonPressed(Button.AB, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
     basic.showLeds(`
         . . # . .
-                . # . # .
-                # . . . #
-                . # . # .
-                . . # . .
-    `)
-    JoyCar.turn(FRDirection.Forward, LRDirection.Left, 21, 2)
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    JoyCar.turn(
+    FRDirection.Forward,
+    LRDirection.Right,
+    49,
+    2
+    )
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
         # . # . #
-                # . # . .
-                # # # . #
-                # . # . #
-                # . # . #
-    `)
+        # . # . .
+        # # # . #
+        # . # . #
+        # . # . #
+        `)
     basic.pause(500)
     basic.showString(" FRIDA")
 })
-basic.forever(function on_forever() {
-    
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    JoyCar.drive(FRDirection.Forward, 49)
+})
+basic.forever(function () {
+	
 })
